1c7050b06a2d8c0557fff8e2634246d09c6db87c
James Wu <ywu@lcs.on.ca>
2019-10-21 14:40:22 -0400

    Initial Commit

---
+++ AGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGuessingGame
+//
+//  Created by James Wu on 2019-10-21.
+//  Copyright © 2019 James Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
